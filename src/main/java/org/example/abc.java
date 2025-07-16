95af47aef37f92596a682231037d505fb8462ea0
bishnu charana sahoo <bishnucharanasahoo1@gmail.com>
2025-07-16 17:55:13 +0530

    asdas

---
+++ src/main/java/org/example/abc.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.testng.annotations.Test;
+
+public class abc {
+    @Test
+    void abc(){
+        System.out.println("bishnu");
+    }
+}
